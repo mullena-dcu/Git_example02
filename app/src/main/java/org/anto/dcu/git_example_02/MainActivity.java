0493d0851b50781e2d77d532ab00fb70903f2090
Anthony Mullen <anthony.mullen23@mail.dcu.ie>
2019-08-08 10:17:01 +0100

    First version of application

---
+++ app/src/main/java/org/anto/dcu/git_example_02/MainActivity.java
@@ -0,0 +1,13 @@
+package org.anto.dcu.git_example_02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
